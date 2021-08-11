864c63a2d6f70b3e59f2a62522e69013e14754db
unknown <slinging.slasher101@gmail.com>
2021-08-11 16:01:21 +0700

    memperbaiki form input data pada page barang

--- src/App.js
+++ src/App.js
@@ -9,57 +9,8 @@ import Barang from "./view/Barang";
 import Kategori from "./view/Kategori";
 import Navbar from "./components/Navbar";
 import {Asside, AssideButton, AssideItem} from './components/Asside';
+import {dasboard,box,kategori} from './components/Icons';
 
-var icons = {
-  dasboard: (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-      />
-    </svg>
-  ),
-  box: (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"
-      />
-    </svg>
-  ),
-  kategori: (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
-      />
-    </svg>
-  ),
-};
 
 
 
@@ -91,19 +42,19 @@ class App extends React.Component {
               <AssideItem
                 onPress={this.assideState}
                 title={"Dashboard"}
-                icon={icons.dasboard}
+                icon={dasboard}
                 to="/"
               />
               <AssideItem
                 onPress={this.assideState}
                 title={"Barang"}
-                icon={icons.box}
+                icon={box}
                 to="/barang"
               />
               <AssideItem
                 onPress={this.assideState}
                 title={"kategori"}
-                icon={icons.kategori}
+                icon={kategori}
                 to="/kategori"
               />
             </div>
--- src/view/Barang.js
+++ src/view/Barang.js
@@ -1,42 +1,68 @@
 import { useTable } from 'react-table';
 import React, { createRef, useEffect, useState } from 'react';
 import { column, bdata } from './BarangProps';
+import { box } from '../components/Icons';
 
 function AddBarangView(props) {
 
     let namaRef = createRef();
     let tipeRef = createRef();
+    let [namaError, fnamaError] = useState(false);
+    let [tipeError, ftipeError] = useState(false);
 
     const addB = () => {
         // console.log(namaRef.current.value,tipeRef.current.value);
-        props.xclick()
-        props.addB({ col1: namaRef.current.value, col2: tipeRef.current.value });
+        let err = false;
+        if (namaRef.current.value === '') {
+            fnamaError(true);
+            err = true;
+        }
+        if (tipeRef.current.value === '') {
+            ftipeError(true);
+            err = true;
+        }
+
+        if (!err) {
+            props.xclick()
+            props.addB({ col1: namaRef.current.value, col2: tipeRef.current.value });
+        }
     }
 
     return (
         <div className={(props.active ? " opacity-100" : "opacity-0 pointer-events-none") + " transition-all flex justify-center items-center fixed top-0 right-0 bottom-0 left-0"}>
 
-            <div className={(props.active ? "transform-none " : "transform translate-y-8 opacity-0") + " transition-all z-10 absolute bg-white rounded-md opacity-100 p-3 w-3/4 md:w-2/4 flex flex-col space-y-4"}>
+            <div className={(props.active ? "transform-none " : "transform translate-y-8 opacity-0") + " transition-all z-10 absolute bg-white rounded-md opacity-100 w-3/4 md:w-2/4 flex flex-col space-y-4"}>
 
-                <button onClick={props.xclick} className={"bg-red-300 font-semibold text-red-800 rounded-full absolute p-3 right-2 transform -translate-y-8"}>
+                {/* <button onClick={props.xclick} className={"bg-red-300 font-semibold text-red-800 rounded-full absolute p-3 right-2 transform -translate-y-5"}>
+
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                     </svg>
-                </button>
+                </button> */}
 
+                <div className={" font-bold text-3xl p-5 border-b flex"}>
+                    <div className={"p-2 bg-red-200 text-red-800  rounded-md mr-3"}>
+                        {box}
+                    </div>
+                    Barang
+                </div>
 
-                <div className={" "}>
+                <div className={" py-3 px-5"}>
                     <label htmlFor="nama" className={" font-semibold"}>Nama</label>
-                    <input ref={namaRef} type="text" id="nama" className={" w-full border px-3 py-2 rounded-md focus:ring-4 focus:border-blue-500 focus:outline-none"} />
+                    <input onFocus={() => fnamaError(false)} ref={namaRef} type="text" id="nama" className={(namaError ? "border-red-500" : "border-gray-300") + " w-full border px-3 py-2 rounded-md focus:ring-4 focus:border-blue-500 focus:outline-none"} />
+                    <small className={(namaError ? "block" : "hidden") + " text-red-500 absolute"}>Nama tidak boleh kosong</small>
                 </div>
-                <div className={" "}>
+                <div className={" py-3 px-5"}>
                     <label htmlFor="nama" className={" font-semibold"}>Tipe</label>
-                    <input ref={tipeRef} type="text" id="nama" className={" w-full border px-3 py-2 rounded-md focus:ring-4 focus:border-blue-500 focus:outline-none"} />
+                    <input onFocus={() => ftipeError(false)} ref={tipeRef} type="text" id="nama" className={(tipeError ? "border-red-500" : "border-gray-300") + " w-full border px-3 py-2 rounded-md focus:ring-4 focus:border-blue-500 focus:outline-none"} />
+                    <small className={(tipeError ? "block" : "hidden") + " text-red-500 absolute"}>Tipe tidak boleh kosong</small>
                 </div>
-                <div className={" "}>
-                    <button onClick={addB} className={" px-3 py-2 font-semibold text-white rounded-md bg-gradient-to-t from-green-500 to-green-400 w-full"}>Save</button>
+                <div className={" justify-end flex bg-gray-200 px-5 py-3 rounded-b-md space-x-3"}>
+                    <button onClick={props.xclick} >Batal</button>
+                    <button onClick={addB} className={" px-3 py-2 font-semibold text-white rounded-md bg-gradient-to-t from-green-500 to-green-400"}>Save</button>
                 </div>
             </div>
+
             <div onClick={props.xclick} className={" w-screen h-screen  fixed bg-gray-600 opacity-50"} />
         </div>
     );
@@ -81,14 +107,14 @@ function Barang(props) {
     return (
         <div className={" shadow-md bg-white px-5 py-10 md:rounded-md flex flex-col text-gray-600"}>
             <div className="flex justify-between space-x-3">
-                <button onClick={() => addbf(!addb)} className={" flex items-center focus:ring-4 ring-green-300 text-sm p-3 font-semibold rounded-full bg-green-200 text-green-800"}>
+                <button onClick={() => addbf(!addb)} className={" flex items-center focus:ring-4 ring-green-300 text-sm p-3 font-semibold rounded-md bg-green-200 text-green-800"}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
                     </svg>
                     Tambah
                 </button>
                 <div className={" flex items-center md:w-5/12 w-full"}>
-                    <input className={" focus:ring-4 focus:border-blue-500 focus:outline-none h-full w-full border rounded-full pl-4 pr-11 "} type="text" />
+                    <input className={" focus:ring-4 focus:border-blue-500 focus:outline-none h-full w-full border rounded-md pl-4 pr-11 "} type="text" />
                     <button className={"  transform -translate-x-12 rounded-full p-1"}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
